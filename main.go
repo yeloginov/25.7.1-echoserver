8793d24641ee5cc6b588ba4afa8461d415b6ff55
yeloginov <yeloginov@gmail.com>
2022-04-10 12:00:18 +0300

    Text mistakes fixed.

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ importgi (
  
 func main() {
     n :=0
-    fmt.Print("Введите целое число: ")
+    fmt.Print("Введите пожалуйста целое число: ")
     _, err := fmt.Scan(&n)
     if err !=nil {
         log.Fatal(err)
     }
-    fmt.Printf("Вы ввели число: %d\n", n)
+    fmt.Printf("Ура! Вы ввели число: %d\n", n)
 }
